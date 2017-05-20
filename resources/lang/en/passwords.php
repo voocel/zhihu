028a79f11735fbd52fafa4157415912175ec0819
voocel <voocel@gmail.com>
2017-05-20 23:40:49 +0800

    password

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
+    'reset' => '你的密码已经重置成功!',
     'sent' => 'We have e-mailed your password reset link!',
     'token' => 'This password reset token is invalid.',
     'user' => "没有找到该邮箱对应的信息",
