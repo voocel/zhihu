0be72877799445ad80412eef29639ed579ba80c6
voocel <voocel@gmail.com>
2017-05-26 23:57:45 +0800

    add

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">这里是展示</div>
+                    <div class="panel-heading">{{$question->title}}</div>
 
                     <div class="panel-body">
-                      哈哈
+                      {!! $question->body !!}
                     </div>
                 </div>
             </div>
