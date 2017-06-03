b8bebbcb1dd8a37939cac21aa38bcbb3ab738690
voocel <voocel@gmail.com>
2017-06-03 23:53:54 +0800

    add

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('email/verify/{token}',['as'=>'email.verify','uses'=>'EmailController@verify']);
-//   // //
+//   // // //
 Route::resource('questions','QuestionsController',['names'=>
     ['create'=>'question.create'],
     ['show9'=>'question.show']
